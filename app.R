9cf4132ae8e2c6bbc3c017972a02d494171d07f0
Pharouq-Sulaiman <141918302+Pharouq-Sulaiman@users.noreply.github.com>
2024-09-27 15:11:34 +0100

    Update app.R

--- app.R
+++ app.R
@@ -42,7 +42,7 @@ options(shiny.host = "0.0.0.0")
 options(shiny.port = 5000)
 
 # Load the dataset
-hospitals <- read.csv("www/healthmopupandbaselinenmisfacility.csv")
+hospitals <- read.csv("https://raw.githubusercontent.com/Pharouq-Sulaiman/geomap.R/refs/heads/main/healthmopupandbaselinenmisfacility.csv")
 options(timeout = 6000000000)
 
 
@@ -260,11 +260,11 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output,session) {
   output$image1 <- renderImage({
-    list(src = "www/medical_logo.jpg", height = 70, width = 100)
+    list(src = "medical_logo.jpg", height = 70, width = 100)
   }, deleteFile = FALSE)
   
   output$image2 <- renderImage({
-    list(src = "www/coat_of_arm.jpg", height = 70, width = 100)
+    list(src = "coat_of_arm.jpg", height = 70, width = 100)
   }, deleteFile = FALSE)
   output$map <- renderLeaflet({
     mapview::mapview(hospitals2)@map
@@ -428,7 +428,7 @@ server <- function(input, output,session) {
   # Generate the leaflet map
   output$map3 <- renderLeaflet({
     
-    hospitalIconUrl <- "www/hospital_plus.jpg" # Replace with the correct URL
+    hospitalIconUrl <- "hospital_plus.jpg" # Replace with the correct URL
     
     # Create a custom icon using the PNG URL
     hospitalIcon <- makeIcon(
